2810b55de8d948969b9e3cdf605e2d00f05774e5
Ronak Nangia <ronak.23544@sscbs.du.ac.in>
2025-06-15 16:30:32 +0530

    updates

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+    baseURL: import.meta.env.VITE_BACKEND_URL
+  ? `${import.meta.env.VITE_BACKEND_URL}/api`
+  : 'http://localhost:5000/api',
     withCredentials: true
 })
 
