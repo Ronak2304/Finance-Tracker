d2fbc24ca3ccf81841d613adcd8738df7c8b554b
Ronak Nangia <ronak.23544@sscbs.du.ac.in>
2025-06-29 23:17:32 +0530

    updated axiosInstance file and simplified the logic

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL
-  ? `${import.meta.env.VITE_BACKEND_URL}/api`
-  : '/api',
-    withCredentials: true
-})
+const backendURL = import.meta.env.VITE_BACKEND_URL || '';
 
-export default axiosInstance
+const axiosInstance = axios.create({
+  baseURL: `${backendURL}/api`,
+  withCredentials: true,
+});
+
+export default axiosInstance;
