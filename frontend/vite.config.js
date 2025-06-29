38eed61110a523e51de2ab8d3e455ab80427a1f7
Ronak Nangia <ronak.23544@sscbs.du.ac.in>
2025-06-29 23:29:39 +0530

    updated viteconfig.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'build' // 👈 important for Render to know where to serve from
+  },
+  base: './', // 👈 optional, useful to avoid broken paths in static hosting
 })
